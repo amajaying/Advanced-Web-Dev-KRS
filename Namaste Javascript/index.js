82407937ebfd3ed8c1b4a0167b426c5deba16d55
Ajay Khatri <amajaying@gmail.com>
2022-11-12 10:50:53 +0530

    first commit

---
+++ Namaste Javascript/index.js
@@ -0,0 +1,9 @@
+// on click button 
+
+var a = "hello";
+var b = 10;
+var c = "bipin";
+
+var ans = a + b + c;
+
+alert(ans);
---
+++ Self Practice/index.js
@@ -0,0 +1,5 @@
+let name = "sachin";
+let age = 44;
+alert(name[0]);
+
+alert(name.replace("sa", "va"));
