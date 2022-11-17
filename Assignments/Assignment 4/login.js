815cf0f015d050e43c24bcb94489e8c6f0180a62
Ajay Khatri <amajaying@gmail.com>
2022-11-17 21:12:32 +0530

    ASSIGNMENT 4

--- Assignments/Assignment 4/login.js
+++ Assignments/Assignment 4/login.js
@@ -1,19 +1,56 @@
 var btn = document.getElementById("loginButton");
 
+var nameList = ["ajay", "aditya", "rishav", "malvika"];
+
 btn.onclick = function() {
-    validate();
+    if (validate()) {
+        alert("You are logged in successfully!");
+    }
 }
 
-console.log(btn)
+
+
+function seterror(id, error) {
+
+    document.getElementById(id).innerHTML = error;
+
+}
+
+
+function clearErrors() {
+    errors = document.getElementsByClassName('error');
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+}
 
 function validate() {
-    var form = document.getElementById("myForm");
-    var username = form.username.value;
-    console.log(username)
-    if (username != "ajay") {
-        var error = document.getElementById("usernameErr");
-        error.innerHTML = "Invalid username";
+    var returnVal = true;
+    clearErrors();
+    var name = document.forms["myForm"]["username"].value;
+
+    if (name == 0) {
+        seterror("usernameErr", "*Name cannot be blank");
+        returnVal = false;
+    } else if (nameList.includes(name)) {
+        returnVal = true;
     } else {
-        alert("Badhai ho!");
+        seterror("usernameErr", "*Enter the correct name");
+        returnVal = false;
     }
+
+    var pass = document.forms['myForm']['password'].value;
+    if (pass.length == 0) {
+        seterror("passwordErr", "*Password cannot be blank");
+        returnVal = false;
+    } else if (pass == "1234") {
+        returnVal = true;
+    } else {
+        seterror("passwordErr", "*Please enter the correct password.");
+        returnVal = false;
+    }
+
+
+    return returnVal;
+
 }
---
+++ Assignments/Assignment 4/register.js
@@ -0,0 +1,109 @@
+var btn = document.getElementById("register");
+var clr = document.getElementById("clear");
+clr.onclick = function() {
+    clearErrors();
+    clearValues();
+}
+
+btn.onclick = function() {
+    var x = validateForm();
+
+    if (x) {
+        alert("Thanks for registering!");
+    }
+}
+
+function clearValues() {
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("gender1").checked = false;
+    document.getElementById("gender2").checked = false;
+    document.getElementById("quali1").checked = false;
+    document.getElementById("quali2").checked = false;
+    document.getElementById("course1").checked = false;
+    document.getElementById("course2").checked = false;
+    document.getElementById("condition-check").checked = false;
+}
+
+function seterror(id, error) {
+
+    document.getElementById(id).innerHTML = error;
+
+}
+
+
+function clearErrors() {
+    errors = document.getElementsByClassName('error');
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+}
+
+function validateForm() {
+    var returnVal = true;
+    clearErrors();
+    var name = document.forms["myForm"]["username"].value;
+
+
+    if (name.length == 0) {
+        seterror("usernameErr", "*Name cannot be blank");
+        returnVal = false;
+    }
+
+
+    if (name.length < 5) {
+        seterror("usernameErr", "*Name must be at least 5 characters long");
+        returnVal = false;
+    }
+
+    var phone = document.forms["myForm"]["phone"].value;
+
+    if (phone.length != 10) {
+        seterror("phoneErr", "*Phone number must be of 10 digits");
+        returnVal = false;
+    }
+
+    var email = document.forms["myForm"]["email"].value;
+    if (email.length == 0) {
+        seterror("emailErr", "*Email cannot be blank");
+        returnVal = false;
+    }
+    if (email.indexOf('@') <= 0) {
+        seterror("emailErr", "*Enter a vaild email");
+        returnVal = false;
+    }
+    var gender = document.forms["myForm"]["gender"].value;
+    if (gender == "") {
+        seterror("genderErr", "*Choose atleast one option");
+        returnVal = false;
+
+    }
+
+    var qualification = document.forms["myForm"]["quali"].value;
+    if (qualification == "") {
+        seterror("qualiErr", "*Choose atleast one option");
+        returnVal = false;
+    }
+
+    var course = document.forms["myForm"]["course"].value;
+    if (course == "") {
+        seterror("courseErr", "*Choose atleast one option");
+        returnVal = false;
+    }
+
+    var country = document.forms["myForm"]["country"].value;
+    if (country == "") {
+        seterror("countryErr", "*Choose atleast one option");
+        returnVal = false;
+    }
+
+    var checkbox = document.forms["myForm"]["condition-check"].checked;
+
+    if (!checkbox) {
+        seterror("conditionsErr", "*Please accept the terms and conditions");
+        returnVal = false;
+    }
+
+    return returnVal;
+}
